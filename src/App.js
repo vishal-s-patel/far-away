a5dc44144d4c5e754d62d75a74e1ebe00abced94
Vishal Patel <vishalpatel1002@yahoo.com>
2023-08-09 22:28:25 +0530

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ export default function App() {
     setItems([...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems}/>
-      <PackingList items={items}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   );
@@ -56,25 +60,25 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({items}) {
+function PackingList({items, onDeleteItem}) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=> onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
