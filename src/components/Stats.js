f1d9dafb7f55c49ace3f23840c1e14ad444f034c
Vishal Patel <vishalpatel1002@yahoo.com>
2023-08-16 22:47:14 +0530

    fix: separated components

---
+++ src/components/Stats.js
@@ -0,0 +1,23 @@
+export default function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+  }
+
+  const totalItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / totalItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 You have ${totalItems} items in your list, and you already packed${" "}
+          ${packedItems} ( ${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
